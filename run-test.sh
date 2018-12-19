c570c31bdbe49b1b635d03d66bcfc98013b60636
Mahesh Jethanandani <mjethanandani@gmail.com>
2018-12-19 13:41:06 -0800

    Changes to address bug#88

--- run-test.sh
+++ run-test.sh
@@ -10,7 +10,6 @@ cd src
 echo "Testing compilation"
 CONFD_OPTS="--fail-on-warnings"
 CONFD_OPTS=""
-confdc -c $CONFD_OPTS -o /opt/confd/etc/confd/etsi-nfv-common.fxs etsi-nfv-common.yang
 confdc -c $CONFD_OPTS -o /opt/confd/etc/confd/etsi-nfv.fxs etsi-nfv.yang
 
 echo "Starting ConfD"
