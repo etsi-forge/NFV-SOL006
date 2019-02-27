1e00418ad50efd779d1f947792008b6a28a544ee
Mahesh Jethanandani <mjethanandani@gmail.com>
2019-02-26 16:58:38 -0800

    Fixed the build for tree diagram

--- .jenkins.sh
+++ .jenkins.sh
@@ -14,6 +14,7 @@ IMG="$IMG_NAME:$VERSION"
 docker build --tag "$IMG" .
 docker run "$IMG"
 
+echo "Building HTML tree"
 docker run -v"src/yang:/yang" mjethanandani/pyang "/bin/sh" -c "pyang -f jstree /yang/*" > "etsi-nfv.html"
 
 exit $?
