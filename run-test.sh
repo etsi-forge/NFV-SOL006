d5e80ad4cca9aa201ba84df31edef93b79c0c094
Mahesh Jethanandani <mjethanandani@gmail.com>
2019-04-17 12:00:08 -0700

    Updates based on feedback from Bruno

--- run-test.sh
+++ run-test.sh
@@ -11,7 +11,7 @@ CONFD_OPTS="--fail-on-warnings"
 CONFD_OPTS=""
 
 echo "Testing compilations of pkg."
-confdc -c $CONFD_OPTS -o /opt/confd/etc/confd/etsi-nfv-pkg.fxs etsi-nfv-pkg.yang
+confdc -c $CONFD_OPTS -o /opt/confd/etc/confd/etsi-nfv-vnfd.fxs etsi-nfv-vnfd.yang
 echo "Testing compilations of descriptors."
 confdc -c $CONFD_OPTS -o /opt/confd/etc/confd/etsi-nfv-descriptors.fxs etsi-nfv-descriptors.yang
 
@@ -19,7 +19,7 @@ echo "Starting ConfD"
 confd
 
 echo "Loading Data for pkg"
-confd_load -l -m nfv-vnf-pkg.xml
+confd_load -l -m nfv-vnfd.xml
 
 echo "Loading Data for descriptors"
 confd_load -l -m nfv.xml
