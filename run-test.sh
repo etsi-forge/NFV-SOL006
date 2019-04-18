ffa3a5cdcc0ce35504b0b70fd9912d03dc958e2a
Mahesh Jethanandani <mjethanandani@gmail.com>
2019-04-18 14:33:33 -0700

    Added module and test cases for PNFD and NSD

--- run-test.sh
+++ run-test.sh
@@ -10,16 +10,26 @@ cd src
 CONFD_OPTS="--fail-on-warnings"
 CONFD_OPTS=""
 
-echo "Testing compilations of pkg."
+echo "Testing compilations of VNFD."
 confdc -c $CONFD_OPTS -o /opt/confd/etc/confd/etsi-nfv-vnfd.fxs etsi-nfv-vnfd.yang
+echo "Testing compilations of PNFD."
+confdc -c $CONFD_OPTS -o /opt/confd/etc/confd/etsi-nfv-pnfd.fxs etsi-nfv-pnfd.yang
+echo "Testing compilations of NSD."
+confdc -c $CONFD_OPTS -o /opt/confd/etc/confd/etsi-nfv-nsd.fxs etsi-nfv-nsd.yang
 echo "Testing compilations of descriptors."
 confdc -c $CONFD_OPTS -o /opt/confd/etc/confd/etsi-nfv-descriptors.fxs etsi-nfv-descriptors.yang
 
 echo "Starting ConfD"
 confd
 
-echo "Loading Data for pkg"
+echo "Loading Data for VNFD"
 confd_load -l -m nfv-vnfd.xml
+
+echo "Loading Data for PNFD"
+confd_load -l -m nfv-pnfd.xml
+
+echo "Loading Data for NSD"
+confd_load -l -m nfv-nsd.xml
 
 echo "Loading Data for descriptors"
 confd_load -l -m nfv.xml
