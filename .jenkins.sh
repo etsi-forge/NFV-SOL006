20e59e94652deff3cfb92289194a797b61767dde
Michele Carignani <michele.carignani@etsi.org>
2019-10-14 18:08:27 +0200

    fail the build only if the compilation fails, remove etsi-nfv-yang from inputs of pyang

--- .jenkins.sh
+++ .jenkins.sh
@@ -16,16 +16,18 @@ CMT=$(git rev-parse --short HEAD)
 docker build --tag "$IMG" .
 docker run "$IMG"
 
+verdict=$?
+
 echo "Building HTML tree"
 docker run -v"$(pwd)/src/yang/:/yang/" mjethanandani/pyang \
 	"/bin/sh" -c "cd /yang/ && pyang -f jstree -p /yang/ \
-	/yang/etsi-nfv.yang > etsi-nfv.html"
+	/yang/*.yang > etsi-nfv.html"
 
 sed -r -i 's|<a href=\"http://www.tail-f.com">|<a href="http://etsi.org">|g' src/yang/etsi-nfv.html
 sed -r -i 's|data\:image/gif\;base64,R0lGODlhS.*RCAA7|https://www.etsi.org/images/ETSILogoTwitter.png\" height=\"100|g' src/yang/etsi-nfv.html
 
-sed -r -i "s|<div class=\"app\">|<p>Commit: <a href=\"https://forge.etsi.org/gitlab/nfv/SOL006/commit/${CMT}\">$CMT</a>, Build:<a href=\"$BUILD_URL\">$BUILD_NUMBER</a></p><div class=\"app\">|g" src/yang/etsi-nfv.html
+sed -r -i "s|<div class=\"app\">|<p>Commit: <a href=\"https://forge.etsi.org/rep/nfv/SOL006/commit/${CMT}\">$CMT</a>, Build:<a href=\"$BUILD_URL\">$BUILD_NUMBER</a></p><div class=\"app\">|g" src/yang/etsi-nfv.html
 
 
-exit $?
+exit $verdict
 
